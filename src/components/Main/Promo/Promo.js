f9750a32d0333ad0b019fd352c2a1e7ef129c9e9
DimkaYmka <mungalov00@icloud.com>
2023-08-18 15:46:26 +0300

    promoLink

--- src/components/Main/Promo/Promo.js
+++ src/components/Main/Promo/Promo.js
@@ -1,12 +1,14 @@
+import { Link } from "react-router-dom";
+
 function Promo() {
   return (
     <section className="promo">
       <div className="promo__text">
-         <h1 className="promo__title">Учебный проект студента факультета <nobr>Веб-разработки</nobr>.</h1>
+        <h1 className="promo__title">Учебный проект студента факультета <nobr>Веб-разработки</nobr>.</h1>
         <p className="promo__subtitle">Листайте ниже, чтобы узнать больше про этот проект и его создателя.</p>
-        <a href="#project" className="promo__link">
-          Узнать больше
-        </a>
+        <Link  className="promo__link" reloadDocument to='#project'>
+            Узнать больше
+        </Link>
       </div>
       <div className='promo__picture'>
       </div>
